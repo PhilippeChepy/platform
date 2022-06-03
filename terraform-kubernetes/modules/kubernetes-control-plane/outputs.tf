cf18740c1fb70d47404c31fff8b1c1ecdf4d5bd2
Philippe Chepy <philippe@chepy.eu>
2022-06-03 21:58:22 +0200

    Implement ingress pools

--- terraform-kubernetes/modules/kubernetes-control-plane/outputs.tf
+++ terraform-kubernetes/modules/kubernetes-control-plane/outputs.tf
@@ -1,12 +1,17 @@
+output "cluster_security_group_id" {
+  description = "A security group ID attached to control plane nodes."
+  value       = exoscale_security_group.cluster.id
+}
+
 output "client_security_group_id" {
   description = "A security group id to add to cluster clients."
   value       = exoscale_security_group.clients.id
 }
 
-output "cluster_ip_address" {
-  value = exoscale_elastic_ip.endpoint.ip_address
-}
-
 output "kubelet_security_group_id" {
   value = exoscale_security_group.kubelet.id
+}
+
+output "cluster_ip_address" {
+  value = exoscale_elastic_ip.endpoint.ip_address
 }
