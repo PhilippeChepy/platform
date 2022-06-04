b09beba053f20e0a6f1e4785d9f0c6e6542d8bc1
Philippe Chepy <philippe@chepy.eu>
2022-06-04 23:58:30 +0200

    Add kas-network-proxy binary extraction script

---
+++ helpers/download-kas-network-proxy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export IMAGE="eu.gcr.io/k8s-artifacts-prod/kas-network-proxy/proxy-server"
+export TAG="v0.0.31"
+export SRC_FILE="proxy-server"
+export DST_FILE="$SRC_FILE-$TAG-linux-amd64"
+
+./extract-from-container-image.sh "$IMAGE" "$TAG" "$SRC_FILE" "$DST_FILE"
