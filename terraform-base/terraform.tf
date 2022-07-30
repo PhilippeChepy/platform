32027b15292394f6f419d9f327e9e5c744666627
Philippe Chepy <philippe@chepy.eu>
2022-07-30 13:29:38 +0200

    Upgrade the exoscale provider to v0.40.0

--- terraform-base/terraform.tf
+++ terraform-base/terraform.tf
@@ -7,7 +7,7 @@ terraform {
 
     exoscale = {
       source  = "exoscale/exoscale"
-      version = ">= 0.39.0"
+      version = ">= 0.40.0"
     }
 
     random = {
--- terraform-kubernetes/modules/kubernetes-kubelet-pool/terraform.tf
+++ terraform-kubernetes/modules/kubernetes-kubelet-pool/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     exoscale = {
       source  = "exoscale/exoscale"
-      version = ">= 0.39.0"
+      version = ">= 0.40.0"
     }
   }
   experiments = [module_variable_optional_attrs]
