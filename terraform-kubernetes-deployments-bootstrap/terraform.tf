a6118d15aa9cd3079e0ce5cdc8b2e6324f9b1ea1
Philippe Chepy <philippe@chepy.eu>
2022-10-07 20:51:49 +0200

    Upgrade hashicorp/kubernetes provider to v2.14.0

--- terraform-kubernetes-deployments-bootstrap/terraform.tf
+++ terraform-kubernetes-deployments-bootstrap/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.13.0"
+      version = ">= 2.14.0"
     }
     local = {
       source  = "hashicorp/local"
