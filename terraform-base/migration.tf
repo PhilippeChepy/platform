8e6b4c21a023cb3294d671485121d922b465c3ee
Philippe Chepy <philippe@chepy.eu>
2022-10-22 21:24:16 +0200

    Remove migrations

--- terraform-base/migration.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = module.ca_certificate.tls_self_signed_cert.root_ca
-  to   = tls_self_signed_cert.root_ca
-}
-
-moved {
-  from = module.ca_certificate.tls_private_key.root_ca
-  to   = tls_private_key.root_ca
-}
