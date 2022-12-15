5d2431301b90b433419a19bbbd1c76bdf678e40a
Philippe Chepy <philippe@chepy.eu>
2022-12-15 19:31:08 +0100

    fix: hashicorp/aws provider configuration

--- terraform-base/providers.tf
+++ terraform-base/providers.tf
@@ -9,7 +9,6 @@ provider "aws" {
 
   # Skip AWS validations
   skip_credentials_validation = true
-  skip_get_ec2_platforms      = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
   skip_region_validation      = true
