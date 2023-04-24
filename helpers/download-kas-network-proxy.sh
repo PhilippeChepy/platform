f6c4587eecbcacc48a7c3f94db4ef9769848ed38
Philippe Chepy <philippe@chepy.eu>
2023-04-24 20:40:59 +0200

    Upgrade konnectivity to v0.1.2

--- helpers/download-kas-network-proxy.sh
+++ helpers/download-kas-network-proxy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export IMAGE="registry.k8s.io/kas-network-proxy/proxy-server"
-export TAG="v0.0.35"
+export TAG="v0.1.2"
 export SRC_FILE="proxy-server"
 export DST_FILE="$SRC_FILE-$TAG-linux-amd64"
 
