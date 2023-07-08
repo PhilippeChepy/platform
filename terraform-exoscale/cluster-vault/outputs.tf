ff3736aab2c832c63f447497071e4d4d88648ec8
Philippe Chepy <philippe@chepy.eu>
2023-07-08 12:29:02 +0200

    (fixes)

--- terraform-exoscale/cluster-vault/outputs.tf
+++ terraform-exoscale/cluster-vault/outputs.tf
@@ -6,10 +6,7 @@ all:
     ansible_ssh_extra_args: "-o StrictHostKeyChecking=no"
     ansible_ssh_private_key_file: artifacts/id_${lower(var.specs.ssh.algorithm)}
     vault_cluster_name: "${local.cluster_name}"
-    #vault_url: "https://${var.internal_nlb.ip_address}:8200"
     vault_ip_address: "${var.internal_nlb.ip_address}"
-    #vault_client_security_group_id: "$ {module.vault_cluster.client_security_group_id}"
-    #vault_server_security_group_id: "$ {module.vault_cluster.server_security_group_id}"
   children:
     vault:
       hosts:
